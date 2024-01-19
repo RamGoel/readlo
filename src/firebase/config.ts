eb872e29e1e406f95761a29b7eb0ca7f51965433
Ram Goel <rgoel766@gmail.com>
2024-01-20 03:19:49 +0530

    fix: updated env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_SENDER_ID,
+    appId: import.meta.env.VITE_USER_ID,
+    measurementId: import.meta.env.VITE_MEASURE_ID
 };
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
